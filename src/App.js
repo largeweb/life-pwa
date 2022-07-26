b5e3c49ed2ca68a0a0cca22b5f467053c25babbe
matt <mat@unc.edu>
2022-07-26 19:15:15 -0400

    committed from script, no message specified

--- src/App.js
+++ src/App.js
@@ -9,14 +9,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { MDBFooter } from 'mdb-react-ui-kit';
 import {Link} from "react-router-dom";
 // import { db } from "./firebase"
-import useLocalStorage from "use-local-storage"
+// import useLocalStorage from "use-local-storage"
+import useLocalStorage from "react-use-localstorage"
 
 function App() {
+
+  const [userInput, setUserInput] = useLocalStorage('userInput', 'empty')
+
   return (
     <div className="App">
       <Navbar />
       <div className='mainmenu'>
         <button className='menubutton'>Button</button>
+        <input
+          type="text"
+          placeholder="Enter your message!"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)} />
+        <div>{userInput}</div>
       </div>
     </div>
   );
