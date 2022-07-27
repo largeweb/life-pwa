61e5fbb5694b91e8ee25f9ae1e13a5cb73144295
matt <mat@unc.edu>
2022-07-27 18:54:05 -0400

    committed from script, no message specified

--- src/pages/Exercise.js
+++ src/pages/Exercise.js
@@ -18,8 +18,18 @@ function Exercise() {
   const [userInput, setUserInput] = useLocalStorage('userInput', 'empty')
 
   // 💪💪💪 EXERCISES 💪💪💪
+  const [miles,setMiles] = useLocalStorage('milescount',0)
   const [pushups, setPushups] = useLocalStorage('pushupcount',0)
   const [pullups, setPullups] = useLocalStorage('pullupcount',0)
+  const [squats,setSquats] = useLocalStorage('pullupcount',0)
+  const [core,setCore] = useLocalStorage('pullupcount',0)
+  const [armcircles,setArmcircles] = useLocalStorage('armcirclescount',0)
+  const [situps,setSitups] = useLocalStorage('situpscount',0)
+  const [lunges,setLunges] = useLocalStorage('lungescount',0)
+  const [butterflys,setButterflys] = useLocalStorage('butterflyscount',0)
+  const [mountainClimbers,setMountainClimbers] = useLocalStorage('mountainClimberscount',0)
+  const [flys,setFlys] = useLocalStorage('flyscount',0)
+
   const [locked, setLocked] = useLocalStorage('lockedbool',true)
 
   return (
@@ -29,12 +39,48 @@ function Exercise() {
         <h1>EXERCISE 💪</h1>
         <Link to={'/'}><button className='menubutton' disabled={locked=="true"}>Back Home</button></Link>
         <div>{userInput}</div>
+        {miles > 0
+          ? <div style={{border:"5px solid gray", background:"cyan"}} value={miles} onClick={(e) => setMiles(parseInt(miles)-parseInt(5))}>MILES ✖️ [ -0.5 ] ✖️ {parseInt(miles)/parseInt(10)}</div>
+          : <div />
+        }
         {pushups > 0
           ? <div style={{border:"5px solid gray", background:"cyan"}} value={pushups} onClick={(e) => setPushups(parseInt(pushups)-parseInt(10))}>PUSHUPS ✖️ [ -10 ] ✖️ {pushups}</div>
           : <div />
         }
         {pullups > 0
           ? <div style={{border:"5px solid gray", background:"cyan"}} value={pullups} onClick={(e) => setPullups(parseInt(pullups)-parseInt(5))}>PULLUPS ✖️ [ -5 ] ✖️ {pullups}</div>
+          : <div />
+        }
+        {squats > 0
+          ? <div style={{border:"5px solid gray", background:"cyan"}} value={squats} onClick={(e) => setSquats(parseInt(squats)-parseInt(30))}>SQUATS ✖️ [ -30 ] ✖️ {squats}</div>
+          : <div />
+        }
+        {core > 0
+          ? <div style={{border:"5px solid gray", background:"cyan"}} value={core} onClick={(e) => setCore(parseInt(core)-parseInt(20))}>CORE ✖️ [ -20 ] ✖️ {core}</div>
+          : <div />
+        }
+        {armcircles > 0
+          ? <div style={{border:"5px solid gray", background:"cyan"}} value={armcircles} onClick={(e) => setArmcircles(parseInt(armcircles)-parseInt(25))}>ARMCIRCLES ✖️ [ -25 ] ✖️ {armcircles}</div>
+          : <div />
+        }
+        {mountainClimbers > 0
+          ? <div style={{border:"5px solid gray", background:"cyan"}} value={mountainClimbers} onClick={(e) => setArmcircles(parseInt(mountainClimbers)-parseInt(20))}>MOUNTAIN CLIMBERS ✖️ [ -20 ] ✖️ {mountainClimbers}</div>
+          : <div />
+        }
+        {situps > 0
+          ? <div style={{border:"5px solid gray", background:"cyan"}} value={situps} onClick={(e) => setSitups(parseInt(situps)-parseInt(25))}>SITUPS ✖️ [ -25 ] ✖️ {situps}</div>
+          : <div />
+        }
+        {lunges > 0
+          ? <div style={{border:"5px solid gray", background:"cyan"}} value={lunges} onClick={(e) => setLunges(parseInt(lunges)-parseInt(10))}>LUNGES ✖️ [ -10 ] ✖️ {lunges}</div>
+          : <div />
+        }
+        {butterflys > 0
+          ? <div style={{border:"5px solid gray", background:"cyan"}} value={butterflys} onClick={(e) => setButterflys(parseInt(butterflys)-parseInt(5))}>BUTTERFLYS ✖️ [ -5 ] ✖️ {butterflys}</div>
+          : <div />
+        }
+        {flys > 0
+          ? <div style={{border:"5px solid gray", background:"cyan"}} value={flys} onClick={(e) => setFlys(parseInt(flys)-parseInt(10))}>30LB. WEIGHTED FLYS ✖️ [ -10 ] ✖️ {flys}</div>
           : <div />
         }
         <hr></hr>
@@ -46,6 +92,14 @@ function Exercise() {
         }
         <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={pushups} onClick={(e) => setPushups(parseInt(pushups)+parseInt(10))}>add pushups</div></div>
         <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={pullups} onClick={(e) => setPullups(parseInt(pullups)+parseInt(5))}>add pullups</div></div>
+        <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={squats} onClick={(e) => setSquats(parseInt(squats)+parseInt(5))}>add squats</div></div>
+        <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={core} onClick={(e) => setCore(parseInt(core)+parseInt(5))}>add core</div></div>
+        <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={armcircles} onClick={(e) => setArmcircles(parseInt(armcircles)+parseInt(5))}>add armcircles</div></div>
+        <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={situps} onClick={(e) => setSitups(parseInt(situps)+parseInt(5))}>add situps</div></div>
+        <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={lunges} onClick={(e) => setLunges(parseInt(lunges)+parseInt(5))}>add lunges</div></div>
+        <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={butterflys} onClick={(e) => setButterflys(parseInt(butterflys)+parseInt(5))}>add butterflys</div></div>
+        <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={mountainClimbers} onClick={(e) => setMountainClimbers(parseInt(mountainClimbers)+parseInt(5))}>add mountainClimbers</div></div>
+        <div><div style={{border:"5px solid gray"}} hidden={locked=="true"} value={flys} onClick={(e) => setFlys(parseInt(flys)+parseInt(5))}>add flys</div></div>
       </div>
       {/* <Footer /> */}
     </div>
