5ddeee5ede8be59f3e1c330dc74e7452f6742bab
matt <mat@unc.edu>
2022-07-31 15:46:42 -0400

    committed from script, no message specified

--- src/pages/AddSomething.js
+++ src/pages/AddSomething.js
@@ -22,7 +22,7 @@ function AddSomething() {
     <div className="App">
       <Navbar />
       <div className='mainmenu'>
-        <h1 style={{fontSize:"40px", fontWeight:"700"}}>📋 Add 📋</h1>
+        <h1 style={{fontSize:"30px", fontWeight:"700"}}>📋 Add 📋</h1>
         {/* <input type="text" style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"100%", height:"200px"}}></input> */}
         <textarea style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"90%", height:"200px"}} onChange={(e) => setAddInput(e.target.value)} value={addInput}></textarea>
         <Link to={'/'}><button className='menubutton' >Back Home</button></Link>
