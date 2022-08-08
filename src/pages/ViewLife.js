c4c07f16e061c91005f17b951637e2f622dfdb89
matt <mat@unc.edu>
2022-08-08 16:03:39 -0400

    committed from script, no message specified

--- src/pages/ViewLife.js
+++ src/pages/ViewLife.js
@@ -36,10 +36,10 @@ function ViewLife() {
       <Navbar />
       <div className='mainmenu'>
         <h1 style={{fontSize:"30px", fontWeight:"700"}}>💻 View 💻</h1>
-        <input type="text" style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"100%", height:"200px"}} onChange={(e) => setFileInput(e.target.data)}></input>
+        <input type="text" style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"100%", height:"100px"}} onChange={(e) => setFileInput(e.target.data)}></input>
         {/* <button className='menubutton' onClick={(e) => setAddInput('todo')}>TODO</button> */}
         <Link to={'/'}><button className='menubutton' >Back Home</button></Link>
-        <Link to={'/'}><button className='menubutton' >View {process.env.REACT_APP_LIFE_DIR}/{fileInput}</button></Link>
+        <button className='menubutton' >View {process.env.REACT_APP_LIFE_DIR}{fileInput}</button>
         <div>{userInput}</div>
       </div>
       {/* <Footer /> */}
