4fcbb850ca87981d6b26f533a0fdb5ea01a303e2
matt <mat@unc.edu>
2022-08-08 16:06:06 -0400

    committed from script, no message specified

--- src/pages/ViewLife.js
+++ src/pages/ViewLife.js
@@ -12,7 +12,7 @@ import useLocalStorage from "react-use-localstorage"
 function ViewLife() {
 
   const [userInput, setUserInput] = useLocalStorage('userInput', 'empty')
-  const [fileInput, setFileInput] = useState('')
+  const [fileInput, setFileInput] = useState('todo')
 
   // useEffect(() => {
   //   showFile(fileInput)
@@ -36,7 +36,8 @@ function ViewLife() {
       <Navbar />
       <div className='mainmenu'>
         <h1 style={{fontSize:"30px", fontWeight:"700"}}>💻 View 💻</h1>
-        <input type="text" style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"100%", height:"100px"}} onChange={(e) => setFileInput(e.target.data)}></input>
+        {/* <input type="text" style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"100%", height:"100px"}} onChange={(e) => setFileInput(e.target.data)}></input> */}
+        <textarea style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"90%", height:"200px"}} onChange={(e) => setFileInput(e.target.value)} value={fileInput}></textarea>
         {/* <button className='menubutton' onClick={(e) => setAddInput('todo')}>TODO</button> */}
         <Link to={'/'}><button className='menubutton' >Back Home</button></Link>
         <button className='menubutton' >View {process.env.REACT_APP_LIFE_DIR}{fileInput}</button>
