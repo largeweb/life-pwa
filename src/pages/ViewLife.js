b9dbdecf5c705d4f58952361833c9edc1399e4cb
matt <mat@unc.edu>
2022-08-08 15:43:46 -0400

    committed from script, no message specified

--- src/pages/ViewLife.js
+++ src/pages/ViewLife.js
@@ -13,7 +13,6 @@ import env from "react-dotenv"
 function ViewLife() {
 
   const [userInput, setUserInput] = useLocalStorage('userInput', 'empty')
-  const [addInput, setAddInput] = useState('')
   const [fileInput, setFileInput] = useState('')
 
   useEffect(() => {
@@ -38,12 +37,11 @@ function ViewLife() {
       <Navbar />
       <div className='mainmenu'>
         <h1 style={{fontSize:"30px", fontWeight:"700"}}>💻 View 💻</h1>
-        {/* <input type="text" style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"100%", height:"200px"}}></input> */}
+        <input type="text" style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"100%", height:"200px"}} onChange={(e) => setFileInput(e.target.data)}></input>
         <button className='menubutton' onClick={(e) => setAddInput('todo')}>TODO</button>
         <Link to={'/'}><button className='menubutton' >Back Home</button></Link>
-        <Link to={'/'}><button className='menubutton' >Back Home</button></Link>
+        <Link to={'/'}><button className='menubutton' >View {env.LIFE_DIR}/{fileInput}</button></Link>
         <div>{userInput}</div>
-        <div>{addInput}</div>
       </div>
       {/* <Footer /> */}
     </div>
