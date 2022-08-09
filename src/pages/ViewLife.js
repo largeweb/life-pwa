de877e7aa5a3fb5a33a85bf7bc8680a30e26fe4a
matt <mat@unc.edu>
2022-08-08 20:07:18 -0400

    committed from script, no message specified

--- src/pages/ViewLife.js
+++ src/pages/ViewLife.js
@@ -16,46 +16,28 @@ function ViewLife() {
 
   const [userInput, setUserInput] = useLocalStorage('userInput', 'empty')
   const [fileInput, setFileInput] = useState('summer/todo')
-  const [fileText, setFileText] = useState('')
+  const [fileText, setFileText] = useState([])
+  const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   fetchLifeText()
-  // }, [fileInput])
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 2000);
+  }, []);
 
-  // const showFile = async () => {
-  //   const lifeFilePath = process.env.REACT_APP_LIFE_DIR.concat(fileInput)
-  //   reader.getText(lifeFilePath)
-  //   .then((data) => {
-  //     console.log(data)
-  //   })
-  // }
   const fetchLifeText = async () => {
-    // Simple GET request using fetch
-    // console.log("function triggered")
-    // fetch('localhost:5000/life')
-    // .then(response => response.json())
-    // .then(data => setFileText(JSON.stringify(data)));
-
-    // const response = await fetch('https://api.npms.io/v2/search?q=react');
-    // const data = await response.json();
-    // setFileText(data.total)
-
-    // const response = await fetch('http://dev-matt.com:5000/life');
-
-    // const requestOptions = {
-    //   method: 'GET',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({ dir: fileInput })
-    // };
-
-    // const response = await fetch('http://localhost:5000/life/' + fileInput + ".txt", {
     const response = await fetch('http://localhost:5000/life', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ dir: fileInput+".txt" })
     });
     const data = await response.json();
-    setFileText(data.lines)
+    let textArr = []
+    for(let i = 0; i < data.lines.length; i++) {
+      console.log(data.lines[i])
+      textArr.push(data.lines[i])
+    }
+    setFileText(textArr)
   }
 
   return (
@@ -63,15 +45,15 @@ function ViewLife() {
       <Navbar />
       <div className='mainmenu'>
         <h1 style={{fontSize:"30px", fontWeight:"700"}}>💻 View 💻</h1>
-        {/* <input type="text" style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"100%", height:"100px"}} onChange={(e) => setFileInput(e.target.data)}></input> */}
         <textarea style={{margin:"5px", border:"5px solid gray", borderRadius:"10%", width:"90%", height:"200px"}} onChange={(e) => setFileInput(e.target.value)} value={fileInput}></textarea>
-        {/* <button className='menubutton' onClick={(e) => setAddInput('todo')}>TODO</button> */}
         <Link to={'/'}><button className='menubutton' >Back Home</button></Link>
         <button className='menubutton'onClick={(e) => fetchLifeText()} >View {process.env.REACT_APP_LIFE_DIR}{fileInput}.txt</button>
         <Link to={'localhost:5000/pull-life'}><button className='menubutton' >PULL CHANGES</button></Link>
         <Link to={'localhost:5000/life'}><button className='menubutton' >GO TO LIFE</button></Link>
         <div>{userInput}</div>
-        <div>{fileText}</div>
+        <div style={{fontSize:"20px", textAlign:"left", marginBottom:"30px", marginLeft:"30px"}}>
+          {fileText.map(txt => <div>{txt}</div>)}
+        </div>
       </div>
       {/* <Footer /> */}
     </div>
