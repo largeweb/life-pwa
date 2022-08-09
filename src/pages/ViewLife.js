a2ec427444dce67f4604db11d1fed20f1667b20a
matt <mat@unc.edu>
2022-08-08 20:20:20 -0400

    committed from script, no message specified

--- src/pages/ViewLife.js
+++ src/pages/ViewLife.js
@@ -35,7 +35,7 @@ function ViewLife() {
     let textArr = []
     for(let i = 0; i < data.lines.length; i++) {
       console.log(data.lines[i])
-      textArr.push(data.lines[i])
+      textArr.push("["+i+"]"+data.lines[i])
     }
     setFileText(textArr)
   }
